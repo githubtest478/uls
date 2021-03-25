b2ab5a87a2608659a89219b6aa5e3c0a86e14c53
Denis Tretiakov <dtretiakov@e1r11p10.unit.ua>
2021-03-25 17:09:31 +0200

    itoa_new

--- src/test.c
+++ src/test.c
@@ -1,134 +1,39 @@
 #include "uls.h"
 
-void uls_algorithm(t_names *s_names)
-{
-    DIR *dir;
-    struct dirent *entry;
+/* new itoa from  −9,223,372,036,854,775,807, TO  +9,223,372,036,854,775,807 */ 
+char *mx_itoa(long long number) {
+    char *str = (char *)malloc(21);
+	int count = 0;
+	unsigned long long num;
+    int flag = 0;
 
-    dir = opendir(".");
-    if(dir == NULL) {
-        perror("uls: ");
+	if(number & 0x8000000000000000) {
+        flag = 1;
+		str[count++] = '-';
+		num = (number ^ 0xFFFFFFFFFFFFFFFF) + 1;
+	}
+	else {
+		num = (unsigned long long) number;
     }
-    if(isatty(STDOUT_FILENO)) {
-        while((entry = readdir(dir)) != NULL) { 
-            if(entry->d_name[0] != '.') {
-                mx_printstr(entry->d_name);
-                mx_printstr(" ");
-            }
+
+	if(num == 0) {
+		str[count++] = '0';
+    }
+
+	for(; num != 0; num /= 10) {
+		str[count++] = ((num % 10) + '0');
+    }
+
+    for(int i = 0; (i + flag) < (count + flag) / 2; ++i) {
+            char temp = str[i + flag];
+            str[i + flag] = str[count - i - 1];
+            str[count - i - 1] = temp;
         }
-        mx_printstr("\n");
-    }
-    else {
-        while((entry = readdir(dir)) != NULL) { 
-            if(entry->d_name[0] != '.') {
-                mx_printstr(entry->d_name);
-                mx_printstr("\n");
-            }
-        }
-    }
-    closedir(dir);
+
+    str[count] = '\0';
+
+    return str;
 }
 
-/* 
-считает количество выделеной памяти для дирекрории 
-*/
 
-char *total_count(char *dir_name) {
-    DIR *folder = NULL;
-    uint32_t sum = 0;
-    struct dirent *entry = NULL;
-    struct stat filestat;
-    char *total;
 
-    folder = opendir(dir_name);  
-    
-    if(folder == NULL) {   
-        perror("Unable to read directory");
-    }
-
-    entry = readdir(folder);
-
-    while(entry) {
-        stat(entry->d_name, &filestat);
-        sum += filestat.st_blocks;
-        entry = readdir(folder);
-    }
-    closedir(folder);
-    char *temp = mx_strjoin("total ", mx_itoa(sum));
-    total = mx_strjoin(temp, "\n");
-    mx_strdel(&temp);
-
-    return total;
-}
-
-/*вывод аналогочный ls -l нужно модифицировать под разные флаги и дирекрории, выводит содержимое !!!ТЕКУЩЕЙ дирекрории но можно передавать просто имя.*/
-void mx_test_open_curent_dir(t_names *names) 
-{
-    DIR *folder = NULL;
-    struct dirent *entry = NULL;
-    struct stat filestat;
-    char *temporary = NULL;
-
-    mx_printstr("total ");
-    temporary = total(".");    // must free
-    mx_printstr(temporary);   
-    free(temporary);           // free
-
-    mx_printstr("\n");
-
-    folder = opendir(".");
-
-    if(folder == NULL) // ERROR
-    {
-        perror("Unable to read directory");
-    }
-    /* Read directory entries */
-    entry = readdir(folder);
-
-    while(entry)
-    {
-        if(entry->d_name[0] != '.') {
-            stat(entry->d_name, &filestat);
-
-            mx_printstr( S_ISDIR(filestat.st_mode) ? "d" : "-");
-            mx_printstr( (filestat.st_mode & S_IRUSR) ? "r" : "-");
-            mx_printstr( (filestat.st_mode & S_IWUSR) ? "w" : "-");
-            mx_printstr( (filestat.st_mode & S_IXUSR) ? "x" : "-");
-            mx_printstr( (filestat.st_mode & S_IRGRP) ? "r" : "-");
-            mx_printstr( (filestat.st_mode & S_IWGRP) ? "w" : "-");
-            mx_printstr( (filestat.st_mode & S_IXGRP) ? "x" : "-");
-            mx_printstr( (filestat.st_mode & S_IROTH) ? "r" : "-");
-            mx_printstr( (filestat.st_mode & S_IWOTH) ? "w" : "-");
-            mx_printstr( (filestat.st_mode & S_IXOTH) ? "x" : "-");
-            mx_printstr(" ");
-          
-            temporary = mx_itoa(filestat.st_nlink); // must free mx_itoa
-            mx_printstr(temporary);       
-            free(temporary);             // free
-            mx_printstr(" ");
-          
-            struct passwd *pass = getpwuid(filestat.st_uid);
-            mx_printstr(pass->pw_name);
-            mx_printstr(" ");
-                 
-            temporary = mx_itoa(filestat.st_gid);       // must free mx_itoa
-            mx_printstr(temporary);          
-            free(temporary);            // free
-            mx_printstr(" ");
-
-            temporary = mx_itoa(filestat.st_size);       // must free mx_itoa
-            mx_printstr(temporary);        
-            free(temporary);             // free
-            mx_printstr(" ");
-
-            time_t modify_time = filestat.st_mtime; 
-            char *a = ctime(&modify_time);  
-            write(1, (a + 4), 12);
-            mx_printstr(" ");
-            mx_printstr(entry->d_name);
-            mx_printstr("\n");
-        }
-        entry = readdir(folder);
-    }
-    closedir(folder);
-}
